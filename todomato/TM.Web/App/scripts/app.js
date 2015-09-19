eafa10de9f51637c73a6539f180434f10d1208bd
messboy000 <messboy000@gmail.com>
2015-09-19 17:18:14 +0800

    [web] 專案初始化 + Angular

---
+++ todomato/TM.Web/App/scripts/app.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+(function () {
+    var app = angular.module("app", ['ngRoute', 'ui.bootstrap', 'timer']);
+    app.config(['$routeProvider', function ($routeProvider) {
+        //路由配置
+        var route = $routeProvider;
+        route.when("/todo/list", { controller: 'todo as t', templateUrl: '/App/views/todo/list.html' });
+        route.when("/", { redirectTo: '/todo/list' });
+        route.otherwise({ templateUrl: '/utils-404' });
+    }
+    ]);
+})();
---
+++ todomato/TM.Web/App/scripts/controllers/TodoController.js
@@ -0,0 +1,81 @@
+﻿(function () {
+
+    angular.module('app')
+        .controller('todo', MainCtrl);
+
+    function MainCtrl($scope, $interval) {
+        var vm = this;
+        vm.todolist = [];
+        vm.tomatolist = [];
+        vm.needTomato = 1;
+        vm.timerSeconds = 0;
+        vm.timeDisplay;
+
+        vm.clickEvent = {
+            addTodo: function () {
+				var newary = [];
+				newary.push({
+					id: "new",
+					todo: vm.todo,
+					needTomato: vm.needTomato,
+					finishTomato: 0
+				});
+				angular.forEach(vm.todolist, function(item, idx){
+				    newary.push(item);
+				});
+				vm.todolist = newary;
+				//初始化
+				vm.todo = "";
+            },
+            finishTodo: function(){
+            	console.log('finish');
+            },
+            startTodo: function(todo){
+                console.log('start');
+                console.log(todo);
+                //計時開始
+                var newary = {};
+            	var max = 1500;
+            	var sec = 60;
+            	var date = new Date();
+            	var startmin = date.getMinutes();
+            	var starthour = date.getHours();
+
+    	        var interval = $interval(function () {
+    	        	sec -= 1;
+    	        	if (sec < 0) {sec = 60};
+    	        	max -= 1;
+		            vm.timerSeconds = (vm.timerSeconds + 1);
+		            vm.timeDisplay = Math.floor((max / 60)) + ':' + (sec % 60);
+	            	console.log(vm.timeDisplay);
+		            //時間終了
+		            if (vm.timerSeconds == 10) {
+		                $interval.cancel(interval);
+
+		            	todo.finishTomato += 1;
+		            	vm.timerSeconds = 0;
+		            	newary.todo = todo.todo
+		            	newary.startmin = startmin;
+		            	newary.starthour = starthour;
+		                vm.tomatolist.push(newary);
+		            }
+		        }, 1000);
+            }
+        }
+
+        
+        vm.todolist.push({
+        	id: "1",
+            todo: "一號事件",
+            needTomato: 3,
+            finishTomato: 0
+        });
+         vm.todolist.push({
+            id: "2",
+            todo: "二號事件",
+            needTomato: 3,
+            finishTomato: 0
+        });
+    }
+
+})()
